bb80477bcf25eaea00d98a3e503cb284030a9aef
Dan Savitzky <dans@surveymonkey.com>
2018-12-21 11:17:05 -0800

    Set up formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
+import numeral from 'numeral'
 
 const ExpenseListItem =  ({ description, amount, createdAt, id }) => (
     <li>
         <p>Description: <Link to={`/edit/${id}`}>{description}</Link></p>
-        <p>Amount: {amount}</p>
-        <p>Created at: {createdAt}</p>
+        <p>Amount: {numeral(amount / 100).format('$0,0.00')}</p>
+        <p>Created at: {moment(createdAt).format('MMMM Do, YYYY')}</p>
     </li>
 )
 
